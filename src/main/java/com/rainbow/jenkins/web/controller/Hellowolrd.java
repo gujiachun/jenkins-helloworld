29c4cd125b1c0264fcdc488c72bee5e8955a18ed
gujiachun <767724839@qq.com>
2017-12-29 16:56:14 +0800

    111

--- src/main/java/com/rainbow/jenkins/web/controller/Hellowolrd.java
+++ src/main/java/com/rainbow/jenkins/web/controller/Hellowolrd.java
@@ -1,15 +1,22 @@
 package com.rainbow.jenkins.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rainbow.jenkins.utils.string.TestStr;
+
+
 @RestController
 public class Hellowolrd {
+	
+	@Autowired
+	private TestStr testStr;
 
 	@GetMapping("/get")
 	@ResponseBody
 	public String get(){
-		return "get";
+		return "get - " + testStr.getStr();
 	}
 }
