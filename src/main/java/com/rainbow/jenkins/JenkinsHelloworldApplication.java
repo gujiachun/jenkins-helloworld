8cd6428ad6ac8fe9a95a469535fa8b283c945e11
gujiachun <767724839@qq.com>
2018-01-04 15:50:58 +0800

    11111

--- src/main/java/com/rainbow/jenkins/JenkinsHelloworldApplication.java
+++ src/main/java/com/rainbow/jenkins/JenkinsHelloworldApplication.java
@@ -2,24 +2,21 @@ package com.rainbow.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 //步骤3：继承SpringBootServletInitializer类
 @SpringBootApplication
 //开启通用注解扫描  
-@ComponentScan
-public class JenkinsHelloworldApplication extends SpringBootServletInitializer{
+//@ComponentScan extends SpringBootServletInitializer
+public class JenkinsHelloworldApplication {
 	
 	/** 
      * 步骤4:实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行 
      */  
-    @Override  
+    /*@Override  
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {  
         builder.sources(this.getClass());  
         return super.configure(builder);  
-    }
+    }*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsHelloworldApplication.class, args);
